fecf72dbb0572ee59a276b533fc883a29db43ae2
126MANGESH <mangeshjadhav126@gmail.com>
2025-11-18 11:35:41 +0530

    f

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, Flask!"
+@app.route("/")
+def home():
+    return "Hello"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
